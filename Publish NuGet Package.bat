154237ff2ce1321d0668b30f485f67bc78f35daa
Adam <adam@adamslair.net>
2019-04-28 18:22:59 +0200

    Updated Package Publish Script

--- Publish NuGet Package.bat
+++ Publish NuGet Package.bat
@@ -1,3 +1,7 @@
+:: search for msbuild make it available
+set msbuildPath=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\
+set PATH=%PATH%;%msbuildPath%
+
 :: make sure we have a clean release build
 msbuild /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" PopupControl.sln
 
@@ -8,7 +12,7 @@ del *.nupkg
 .nuget\nuget pack PopupControl\PopupControl.csproj -Properties Configuration=Release
 
 :: upload the nuget packages
-.nuget\nuget push *.nupkg
+.nuget\nuget push *.nupkg -Source "https://www.nuget.org"
 
 :: remove nupkg files after uploading them
 del *.nupkg
